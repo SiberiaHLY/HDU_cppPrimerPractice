9ef7467458842ffc48f13ada885bca67413b4f9a
SiberiaHLY <413512110@qq.com>
2019-09-18 17:10:01 +0800

    Create 6_10.h

---
+++ src/chapter6/6_10.h
@@ -0,0 +1,10 @@
+/*
+# Copyright (c) 2019 Luyu Hui. All rights reserved.
+*/
+
+#ifndef SRC_CHAPTER6_6_10_H_
+#define SRC_CHAPTER6_6_10_H_
+
+void mySwap(int *a, int *b);
+
+#endif  // SRC_CHAPTER6_6_10_H_
