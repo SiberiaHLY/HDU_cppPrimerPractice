763de494bb707ce9ef833af1860df799cbe2d9c9
SiberiaHLY <413512110@qq.com>
2019-09-18 11:58:52 +0800

    Create 3_20.cpp

---
+++ src/chapter3/3_20.cpp
@@ -0,0 +1,19 @@
+/*
+# Copyright (c) 2019 Luyu Hui. All rights reserved.
+*/
+
+#include<iostream>
+#include<vector>
+
+int main() {
+    std::vector<int> v;
+    int tempInt;
+    while(std::cin >> tempInt) {
+        v.push_back(tempInt);
+    }
+    //输出相邻整数和
+    for(auto it:v) {
+        if( it == v.end())
+    }
+    return 0;
+}
