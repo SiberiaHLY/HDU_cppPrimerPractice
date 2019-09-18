480e5fb2a7c808696bcdf69e62b94be9a3139262
SiberiaHLY <413512110@qq.com>
2019-09-18 16:29:22 +0800

    Update 3_20.cpp

--- src/chapter3/3_20.cpp
+++ src/chapter3/3_20.cpp
@@ -7,13 +7,26 @@
 
 int main() {
     std::vector<int> v;
+    std::vector<int>::iterator it;
     int tempInt;
-    while(std::cin >> tempInt) {
+    while (std::cin >> tempInt) {
         v.push_back(tempInt);
     }
-    //输出相邻整数和
-    for(auto it:v) {
-        if( it == v.end())
+    // 输出相邻整数和
+    for (it = v.begin(); it != v.end()-1; it++) {
+        std::cout << *it + *(it+1) << std::endl;
+    }
+    // 输出头尾和
+    int vectorSize = v.size();
+    if (vectorSize%2) {
+        for (int i = 0; i < vectorSize/2; i++) {
+            std::cout << v[i]+v[vectorSize-1-i] << std::endl;
+        }
+        std::cout << v[vectorSize/2] << std::endl;
+    } else {
+        for (int i = 0; i < vectorSize/2; i++) {
+            std::cout << v[i]+v[vectorSize-1-i] << std::endl;
+        }
     }
     return 0;
 }
